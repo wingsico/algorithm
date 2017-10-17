91cb8638b24e0d48da14b17a7b9a85c26179bc62
wingsico <wingsico@outlook.com>
2017-10-17 20:00:55 +0800

    Initial Commit

---
+++ acm_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acm_test
+//
+//  Created by 钟伟杰 on 2017/10/17.
+//  Copyright © 2017年 钟伟杰. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
